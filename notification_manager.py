53417a294399f9a200b1a2e2e0696c0b61560645
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-06-11 21:39:02 +0530

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-ACCOUNT_ID = "ACe6c9ff434ad5a8cf19e2a2772247b65f"
-AUTH_TOKEN = "9cfc444224ef4569c2249b3fcf602422"
+ACCOUNT_ID = "TWILIO_ACCOUNT_ID"
+AUTH_TOKEN = "TOKEN"
 
 
 class NotificationManager:
@@ -14,7 +14,7 @@ class NotificationManager:
         message = self.client.messages.create(
            body=message,
            from_="+19786784065",
-           to="+919431447302"
+           to="WRITE_NUMBER"
         )
         print(message.sid)
 
